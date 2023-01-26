0ea2b02337afd74040dcf7067aa85bf60bb30449
Satronic <electronsgm@gmail.com>
2023-01-25 17:42:51 -1200

    CONFIG(root): Initial template config

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { View, Text } from 'react-native';
+
+const App = () => {
+
+
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Productos</Text>
+      </View>
+    </NavigationContainer>
+  );
+}
+
+export default App;
